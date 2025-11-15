c77f9f4ac5a854ec61ca7ee9f7dc8cfab0fdfa6c
Agustin <agus_1404@live.com>
2025-11-15 06:41:04 -0300

    Proyecto Veterinaria el Ceibo- Primer commit

---
+++ VeterinariaElCeibo/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using VeterinariaElCeibo.Models; // <-- importante
+
+namespace VeterinariaElCeibo.Data
+{
+    // Ahora el contexto usa ApplicationUser como tipo de usuario
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Más adelante acá vamos a agregar:
+        // public DbSet<Cliente> Clientes { get; set; }
+        // public DbSet<Mascota> Mascotas { get; set; }
+        // etc.
+    }
+}
---
+++ VeterinariaElCeibo/Models/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace VeterinariaElCeibo.Models
+{
+    // Usuario de la aplicación (extiende el usuario base de Identity)
+    public class ApplicationUser : IdentityUser
+    {
+        // NO los marco como [Required] todavía para no romper la migración
+        // más adelante podemos validarlos desde los formularios.
+
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? DNI { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+    }
+}
